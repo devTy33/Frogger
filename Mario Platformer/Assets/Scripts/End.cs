d74720ad0e05a4c06fb694f3552469b20e711d8a
RileyTaylor908 <rileytaylor908@gmail.com>
2023-05-04 12:32:50 -0400

    Mario Platformer

---
+++ Mario Platformer/Assets/Scripts/End.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class End : MonoBehaviour
+{
+    private bool levelComplete = false;
+    private void Start()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+            if (collision.gameObject.name == "Player" && !levelComplete) {
+                levelComplete = true;
+                CompleteLevel();
+            }
+    }
+    
+    private void CompleteLevel() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+    }
+}
---
+++ Mario Platformer/Assets/Scripts/PlayerDeath.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerDeath : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Animator ani;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        ani = GetComponent<Animator>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.CompareTag("Trap")) {
+            Die();
+        }
+    }
+
+    private void Die() {
+        rb.bodyType = RigidbodyType2D.Static;
+        ani.SetTrigger("death");
+    }
+
+    private void RestartLevel() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
